57b54b2707b97a196a32c47053a76794258d2fc3
Suraj Kumar <suraj.kr1450@gmail.com>
2022-06-24 10:57:04 +0530

    zira working....

--- Module3/Jira/script.js
+++ Module3/Jira/script.js
@@ -1,6 +1,8 @@
 let addBtn = document.querySelector(".add-btn");     // isse button select ho jaega
 let modal = document.querySelector(".modal-cont");   
-let taskArea = document.querySelector(".textarea-cont");  
+let taskArea = document.querySelector(".textarea-cont"); 
+let mainCont =  document.querySelector(".main-cont");
+let allPriorityColor =  document.querySelectorAll(".priority-color");
 let addModal = true;
 addBtn.addEventListener("click", function(){
     // console.log("Button is clicked");
@@ -14,6 +16,15 @@ addBtn.addEventListener("click", function(){
     }
     addModal = !addModal;
 }) 
+
+for(let  i = 0; i < allPriorityColor.length; i++){
+    let PriorityDivOneColor = allPriorityColor[i];
+    PriorityDivOneColor.addEventListener("click", function(){
+        PriorityDivOneColor.classList.add("active");
+})
+}
+
+
 modal.addEventListener('keydown',function(e){
 let key = e.key;
 //console.log(key);
@@ -28,6 +39,27 @@ if(key == 'Enter'){
 
 
 
-function createTicket(){
-    console.log("create ticket");
+function createTicket(task){
+    // 
+    
+//     <div class="ticket-cont">
+//     <div class="ticket-color black"></div>
+//     <div class="ticket-id">#dfrwgv443</div>
+//     <div class="task-area">Task</div>
+//   </div>
+
+
+let ticketCont = document.createElement("div");
+ticketCont.setAttribute('class','ticket-cont' );
+ticketCont.innerHTML = `<div class="ticket-cont">
+                 <div class="ticket-color black"></div>
+                 <div class="ticket-id">#dfrwgv443</div>
+                 <div class="task-area">${task}</div>
+                 </div>`
+
+
+    mainCont.appendChild(ticketCont);             
+
+
+
 }
