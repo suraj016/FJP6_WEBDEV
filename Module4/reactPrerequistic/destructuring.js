c438f02bcb3f450bc5e121f0695a972be524724c
Suraj Kumar <suraj.kr1450@gmail.com>
2022-07-21 10:54:54 +0530

    destructuring in react

--- Module4/reactPrerequistic/destructuring.js
+++ Module4/reactPrerequistic/destructuring.js
@@ -20,20 +20,50 @@
 
 
 
-let obj = {
-    name  : "Adam Kumar",
-    state : "New York",
-    pincode : 132332
-}
+// let obj = {
+//     name  : "Adam Kumar",
+//     state : "New York",
+//     pincode : 132332
+// }
 
 // let {name, pincode} = obj;
 // console.log(name);
 // console.log(pincode);
 
 
-let {state , name:firstName} = obj;
-console.log(firstName);
-console.log(state);
+// let {state , name:firstName} = obj;
+// console.log(firstName);
+// console.log(state);
+
+
+
+
+//            IN CASE OF NESTED OBJECT
+
+
+let obj = {
+    name  : "Adam Kumar",
+   address:{
+    country : "Usa",
+    state : {
+        state : "New York",
+        pincode : 132332
+    }
+   }
+    
+}
+
+
+let {name} = obj;
+console.log(name);
+
+let {address: {country:cd}} = obj;  // SYNTAX
+console.log(cd);
+
+let {address:{state:{pincode}}}   = obj;  // HYRACHY WISE 
+console.log(pincode);
+
+
 
 
 
