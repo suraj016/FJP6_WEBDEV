2b79d78d03cd58cf5333aa2faaa2081f8319cf13
Suraj Kumar <suraj.kr1450@gmail.com>
2022-07-22 11:58:36 +0530

    this.js

--- Module4/reactPrerequistic/this.js
+++ Module4/reactPrerequistic/this.js
@@ -1,1 +1,31 @@
-console.log(this);  /// THIS WILL GIVE EMPTY OBJECT
+//  THIS KEYWORD IS FOR REFRENCE PURPOSE TO ANY OBJECT
+//  THIS KEYWORD VALUE WILL DEPEND UPON HOW IT IS CALLED ?
+// 1. FUNCTION INVOCATION
+// 2. METHOD INVOCATION
+
+
+console.log(this);  /// THIS WILL GIVE EMPTY OBJECT  {}
+
+function abc(){
+    console.log("This inside a function",this);
+}
+
+abc();    // FUNCTION INVOCATION
+// console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+let obj  = {
+    name : "Aman",
+    age : 23,
+    test : function(){
+        this.def();
+    },
+    def : function(){     // DEF EK METHOD HAE
+        console.log(this)
+    }
+}
+obj.def();   // METHOD INVOCATION
+
+// console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+// let ghi = obj.def;
+// ghi();   // FUNCTION INVOCATION
