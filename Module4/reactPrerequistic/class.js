a9644b1009e84e7e54b32b8620c9874c938f82c0
Suraj Kumar <suraj.kr1450@gmail.com>
2022-07-31 21:08:42 +0530

    class.ja

---
+++ Module4/reactPrerequistic/class.js
@@ -0,0 +1,39 @@
+class Person{
+    constructor(name,age){
+       this.name = name,
+       this.age = age
+    }
+
+    welcome(){
+        console.log("hello"+ this.name);
+    }
+}
+
+
+class Teacher extends Person
+{
+    constructor(name,age,classStrenght){
+        super(name,age);
+        this.classStrenght = classStrenght;
+    }
+}
+
+class Student extends Person{
+    constructor (name, age , cgpa){
+        super(name,age),  // super parent ka constructor hae usse call kar rehe hae
+        this.cgpa = cgpa;
+    }
+}
+
+
+
+
+    let person = new Person ("Adam", 23);
+console.log(person);
+
+
+let teacher = new Teacher("Aman", 23,45);
+console.log(teacher);
+
+let student  = new Student ("Suraj", 23,9);
+console.log(student);
--- Module4/reactPrerequistic/constructorFn.js
+++ Module4/reactPrerequistic/constructorFn.js
@@ -1,4 +1,6 @@
 function car(Name,Model,Color){
+
+    console.log(this);
     this.name = Name;
     this.model = Model;
     this.color = Color;
@@ -11,4 +13,9 @@ function car(Name,Model,Color){
 
 
 let car1 = new car('BMW','X6','WHITE');
-console.log(car1);
+//console.log(car1);
+//car1.test();
+
+let car2 = new car('MERCEDES','S CLASS','RED');
+//console.log(car2);
+
