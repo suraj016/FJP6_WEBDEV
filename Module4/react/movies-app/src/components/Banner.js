76a752d389f03de7e3c58758cfba0c7831cc73b4
Suraj Kumar <suraj.kr1450@gmail.com>
2022-08-15 11:35:20 +0530

    banner done

--- Module4/react/movies-app/src/components/Banner.js
+++ Module4/react/movies-app/src/components/Banner.js
@@ -4,13 +4,13 @@ import { Component } from "react";
 class Banner extends Component {
     render() {
         return (
-            <div className="card" >
-                <img src="https://sm.ign.com/ign_in/review/m/marvels-sp/marvels-spider-man-miles-morales-review_fkqf.jpg" className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
+            <div className="card banner-card" >
+                <img src="https://sm.ign.com/ign_in/review/m/marvels-sp/marvels-spider-man-miles-morales-review_fkqf.jpg" className="card-img-top banner-img" alt="..."/>
+                   
+                        <h1 className="card-title banner-title">spider man no way home</h1>
+                        <p className="card-text banner-text">Spider-Man: No Way Home is a 2021 American superhero film based on the Marvel Comics character Spider-Man, co-produced by Columbia Pictures and Marvel Studios and distributed by Sony Pictures Releasing. I</p>
+                      
+                    
             </div>
         )
     }
