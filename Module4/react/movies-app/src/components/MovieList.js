31ffffb3c62051cbf3decf5ce6c8d98033ad395f
Suraj Kumar <suraj.kr1450@gmail.com>
2022-08-15 20:51:49 +0530

    movielist component added

--- Module4/react/movies-app/src/components/MovieList.js
+++ Module4/react/movies-app/src/components/MovieList.js
@@ -6,28 +6,38 @@ import {movies} from "../movieData";
 class MovieList extends Component{
     render(){
        let moviesArr =   movies.results
+
        return(
         <>
+       
+       <div>
+                    <h3 className="text-center"><strong>Trending</strong></h3>
+                </div>
 
+          <div className="movies-list">
+             {moviesArr.map((movieEle) => {
 
- 
-<div>
-           <h3 className=""><strong>Trending</strong></h3>
-           </div>
-
-            <div>
-            {moviesArr.map((movieELe) => (
-            <h5>{movieELe.original_title}</h5>
-           ))}
-            </div>
-
-
-           
-          
+                return(
+                    <div className="card movie-card">
+                    <img src="https://www.cnet.com/a/img/resize/c2372505b1ec497b5e8f397f9ff807f8ef4438c3/2021/12/14/76af5f8b-4ae5-4eae-b4d7-b5ff968ef9fa/spider-man-no-way-home-osx1440-comp-v005-300dpi-1003.jpg?auto=webp&fit=crop&height=675&width=1200" style = {{heigth: '40vh', width: '20vw'}} className="card-img-top movie-img" alt="..."/>
+                     <div className="card-body">
+                      <h5 className="card-title">{movieEle.original_title}</h5>
+                      
+  
+                      </div>
+                  </div>
+                )
+                
+                  
+                
+             })}
+          </div>
+              
         </>
-
        )
+
     }
-}
+       
+    }    
 
 export default MovieList;
