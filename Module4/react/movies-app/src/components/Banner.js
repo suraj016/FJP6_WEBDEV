c76e82db45bc216c13fbf1e3940b64986a9c8ca0
Suraj Kumar <suraj.kr1450@gmail.com>
2022-08-21 12:37:48 +0530

    added the pagination in footer section

--- Module4/react/movies-app/src/components/Banner.js
+++ Module4/react/movies-app/src/components/Banner.js
@@ -4,7 +4,7 @@ import {movies} from "../movieData";
 
 class Banner extends Component {
     render() {
-        let movie = movies.results[0];
+        let movie = movies.results[Math.floor(Math.random() * 10)];
         let backdrop_path = movie.backdrop_path;
        // console.log(backdrop_Path);
         return (
--- Module4/react/movies-app/src/components/MovieList.js
+++ Module4/react/movies-app/src/components/MovieList.js
@@ -32,6 +32,21 @@ class MovieList extends Component{
                 
              })}
           </div>
+
+          <div style = {{display:"flex", justifyContent: "center"}}>
+          <nav aria-label="Page navigation example">
+  <ul className="pagination">
+    <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+    <li className="page-item"><a className="page-link" href="#">1</a></li>
+    <li className="page-item"><a className="page-link" href="#">2</a></li>
+    <li className="page-item"><a className="page-link" href="#">3</a></li>
+    <li className="page-item"><a className="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+          </div>
+
+
+          
               
         </>
        )
