232ec2a2efec85810823d0db4b6a6cb49d08c5e5
Suraj Kumar <suraj.kr1450@gmail.com>
2022-09-10 21:42:37 +0530

    favourite list and all genre selected

--- Module4/react/movies-app/src/components/Fav.js
+++ Module4/react/movies-app/src/components/Fav.js
@@ -1,34 +1,93 @@
 import { Component } from "react";
-import {movies} from '../movieData';
+
 
 class Fav extends Component {
-  render() {
-   const moviesArr = movies.results
-        console.log(moviesArr)
-        let genreIds = { 28: "Action", 12: "Adventure", 16: "Animation", 35: "Comedy", 80: "Crime", 99: "Documentary", 18: "Drama", 10751: "Family", 14: "Fantasy", 36: "History", 27: "Horror", 10402: "Music", 9648: "Mystery", 10749: "Romance", 878: "Science Fiction", 10770: "TV Movie", 53: "Thriller", 10752: "War", 37: "Western"}
-    
-        let tempArr = [];
-        tempArr.push("All genres")
-        moviesArr.map((movieobj)=>{
-          if(!tempArr.includes(genreIds[movieobj.genre_ids[0]])){
-tempArr.push(genreIds[movieobj.genre_ids[0]])
-          }
-        })
+  constructor() {
+    super();
+    this.state = {
+      genres: [],
+      currgenre: "All genres",
+      movies: []
+      // movies2: [],
+      // currText: "",
+    };
+  }
 
+  componentDidMount() {
+    let genreIds = {
+      28: "Action",
+      12: "Adventure",
+      16: "Animation",
+      35: "Comedy",
+      80: "Crime",
+      99: "Documentary",
+      18: "Drama",
+      10751: "Family",
+      14: "Fantasy",
+      36: "History",
+      27: "Horror",
+      10402: "Music",
+      9648: "Mystery",
+      10749: "Romance",
+      878: "Science Fiction",
+      10770: "TV Movie",
+      53: "Thriller",
+      10752: "War",
+      37: "Western",
+    };
+    let data = JSON.parse(localStorage.getItem("movies-app") || "[]"); //movies
+    let tempArr = [];
+    tempArr.push("All genres");
+    data.map((movieObj) => {
+      if (!tempArr.includes(genreIds[movieObj.genre_ids[0]])) {
+        tempArr.push(genreIds[movieObj.genre_ids[0]]);
+      }
+    });
+    this.setState({
+      movies: [...data],
+     // movies2: [...data],
+      genres: [...tempArr],
+    });
+  }
+
+  render() {
+
+    let genreIds = {
+      28: "Action",
+      12: "Adventure",
+      16: "Animation",
+      35: "Comedy",
+      80: "Crime",
+      99: "Documentary",
+      18: "Drama",
+      10751: "Family",
+      14: "Fantasy",
+      36: "History",
+      27: "Horror",
+      10402: "Music",
+      9648: "Mystery",
+      10749: "Romance",
+      878: "Science Fiction",
+      10770: "TV Movie",
+      53: "Thriller",
+      10752: "War",
+      37: "Western",
+    };
+  
 
     return (
       <div className="container">
         <div className="row">
           <div className="col-3">
             <ul className="list-group genre-selector">
-
-                {
-                   tempArr.map((genre)=>(
-                      <li className="list-group-item">{genre}</li>
-                       ))
-                }
-              
-             
+              {
+               this.state.genres.map((genre)=>(
+                this.state.currgenre == genre ?(
+                    <li className="list-group-item active" >{genre}</li>
+                ):
+                (<li className="list-group-item" >{genre}</li>)
+            ))
+              }
             </ul>
           </div>
           <div className="col-9 fav-table">
@@ -43,27 +102,34 @@ tempArr.push(genreIds[movieobj.genre_ids[0]])
 
             <table className="table">
               <thead>
-                                <tr>
-                
-                                    <th scope="col">Title</th>
-                                    <th scope="col">Genre</th>
-                                    <th scope="col">Popularity</th>
-                                    <th scope="col">Rating</th>
-                                    <th scope="col">Delete</th>
-                                </tr>
+                <tr>
+                  <th scope="col">Title</th>
+                  <th scope="col">Genre</th>
+                  <th scope="col">Popularity</th>
+                  <th scope="col">Rating</th>
+                  <th scope="col">Delete</th>
+                </tr>
               </thead>
               <tbody>
-              {
-                                    moviesArr.map((movieEle)=>(
-                                        <tr>
-                                            <th scope="row"><img  style={{width:"8rem",padding:"1rem"}} src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`}/>{  movieEle.title}</th>
-                                            <td>{genreIds[movieEle.genre_ids[0]]}</td>
-                                            <td>{movieEle.popularity}</td>
-                                            <td>{movieEle.vote_average}</td>
-                                            <td><button type="button" className="btn btn-danger">Damger</button></td>
-                                        </tr>
-                                    ))
-                }
+                {this.state.movies.map((movieEle) => (
+                  <tr>
+                    <th scope="row">
+                      <img
+                        style={{ width: "8rem", padding: "1rem" }}
+                        src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`}
+                      />
+                      {movieEle.title}
+                    </th>
+                    <td>{genreIds[movieEle.genre_ids[0]]}</td>
+                    <td>{movieEle.popularity}</td>
+                    <td>{movieEle.vote_average}</td>
+                    <td>
+                      <button type="button" className="btn btn-danger">
+                        Damger
+                      </button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
 
@@ -95,9 +161,8 @@ tempArr.push(genreIds[movieobj.genre_ids[0]])
 
 export default Fav;
 
-
 // react router dom
 
 // different url pe different component
 
-// route-> 2 cheez chaiye 1.url  2 
+// route-> 2 cheez chaiye 1.url  2
